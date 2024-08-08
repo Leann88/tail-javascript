8504e34c70217106d1f350fe9f60c913a6575d68
Leann Labelle <llabelle.088@gmail.com>
2024-08-08 17:16:08 -0400

    initial commit

---
+++ tail.js
@@ -0,0 +1,40 @@
+import fs from "fs";
+import readline from "readline";
+
+let stream;
+let linesToPrint = 10;
+let arr = [];
+let params = process.argv.slice(2);
+let file = params.slice(params.length - 1);
+
+
+for(let i  = 0; i < params.length - 1; i++) {
+    let hasIncorrectParam = false;
+    switch(params[i]) {
+    case "-n":
+        if(isNaN(params[i+1])) throw new Error("invalid input");
+        linesToPrint = Number(params[i+1])
+        i++;
+        break;
+    default:
+        throw new Error(`${params[i]} not found`);
+    }
+
+    if(hasIncorrectParam) break;
+}
+
+stream = fs.createReadStream(file[0]);
+
+let rl = readline.createInterface({
+    input: stream,
+    terminal: false
+});
+    
+rl.on('line', function(line) {
+    if(arr.length === linesToPrint) arr.shift();
+    arr.push(line);
+});
+
+rl.on('close', () => {
+    console.log(arr.join("\n"));
+})
